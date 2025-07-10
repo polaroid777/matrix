1ddc0fdfeea3e67ad1b28c97a4d985a67a268ca4
MD <mdzoban@reenbit.com>
2025-07-10 17:55:18 +0300

    added new custom element; created new html to build simple landing page and learn css;

--- index.js
+++ index.js
@@ -18,3 +18,36 @@ class CounterElement extends HTMLElement {
 }
 
 customElements.define('my-counter', CounterElement);
+
+
+class MyNewTestElement extends HTMLElement {
+	constructor(){
+		super();
+		this.attachShadow({ mode: 'open'});
+
+		this.shadowRoot.innerHTML = `
+		<style>
+			p{
+				margin: 0;
+				padding: 0.5rem 1rem;
+				background-color:rgb(210, 236, 247);
+				color:rgb(14, 28, 32);
+				border-radius: 0.5rem;
+				border-left: 4px solid #264653;
+				box-shadow: 0px 4px 8px 0px rgb(26, 33, 36, 0.2);
+
+				position: fixed;
+				z-index: 50;
+				bottom: 5px;
+				left: 5px;
+			}
+		</style>
+
+		<p>
+			<slot>Hello USER!</slot>
+		</p>
+		`
+	}
+}
+
+customElements.define('info-element', MyNewTestElement)
